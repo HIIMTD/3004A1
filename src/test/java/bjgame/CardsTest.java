fafd00647d9f3a6d0aa050000c226abd072f13fb
HIIMTD <td950614@gmail.com>
2018-09-15 03:13:37 -0400

    test:add junit test in CardsTest.java

--- src/test/java/bjgame/CardsTest.java
+++ src/test/java/bjgame/CardsTest.java
@@ -3,5 +3,20 @@ package bjgame;
 import junit.framework.TestCase;
 
 public class CardsTest extends TestCase{
-
+	
+	public void testConstructor() {
+		Cards card = new Cards(Cards.Suit.S,11);
+		assertEquals(11, card.getRank);
+		assertEquals(Cards.Suit.S, card.getSuit);
+	}
+	
+	public void testToString() {
+		Cards card = new Cards(Cards.Suit.H,4);
+		assertEquals("H4", card.toString());
+	}
+	
+	public void testToString() {
+		Cards card = new Cards(Cards.Suit.D,13);
+		assertEquals("DK", card.toString());
+	}
 }
