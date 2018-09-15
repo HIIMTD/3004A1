b36c72885bd0177001913cba08c2c87553127890
HIIMTD <td950614@gmail.com>
2018-09-15 16:23:11 -0400

    test: DeckTest.java created

--- src/test/java/bjgame/DeckTest.java
+++ src/test/java/bjgame/DeckTest.java
@@ -4,4 +4,22 @@ import junit.framework.TestCase;
 
 public class DeckTest extends TestCase{
 
+		public void testCons() {
+			Deck deck = new Deck();
+			assertEquals(0, deck.size());
+		}
+		
+		public void testCreateDeck() {
+			Deck deck = new Deck();
+			deck.createDeck();
+			assertEquals(52, deck.size());
+		}
+		
+		public void testShuffle() {
+			Deck deck1 = new Deck();
+			Deck deck2 = new Deck();
+			assertTure(deck1.equals(deck2));
+			deck2.shuffle();
+			assertFalse(deck1.equals(deck2);
+		}
 }
