74294be94d591c4f0f004f18f6bbc5790fff4058
HIIMTD <td950614@gmail.com>
2018-09-15 21:27:37 -0400

    add Hand

--- src/test/java/bjgame/HandTest.java
+++ src/test/java/bjgame/HandTest.java
@@ -7,8 +7,18 @@ import org.junit.Test;
 public class HandTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsBackJack() {
+		Hand hand = new Hand();
+		hand.addCard(new Card(CardSuit.C, CardRank.ACE));
+		assertFalse(hand.isBlackJack());
+		
+		hand.addCard(new Card(CardSuit.S, CardRank.KING));
+		assertTrue(hand.isBlackJack());
+	}
+	
+	@Test
+	public void testIsBusted() {
+		
 	}
 
 }
